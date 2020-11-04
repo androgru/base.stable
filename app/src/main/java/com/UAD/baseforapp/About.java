88e067edd8665b5004f400212f9dce0a0c3fe577
androgru <aljithchonedanpopup@gmail.com>
2020-11-04 11:13:01 +0530

    first commit

---
+++ app/src/main/java/com/UAD/baseforapp/About.java
@@ -0,0 +1,49 @@
+package com.UAD.baseforapp;
+
+import android.content.*;
+import android.graphics.*;
+import android.os.*;
+import android.support.v7.app.*;
+import android.support.v7.widget.*;
+import android.view.*;
+public class About extends ActionBarActivity
+{  
+
+	private Toolbar app_bar;
+
+	@Override 
+	protected void onCreate(Bundle savedInstanceState) {  super.onCreate(savedInstanceState);
+		setContentView(R.layout.about_toolbar);
+		app_bar=(Toolbar) findViewById(R.id.app_bar);
+		setSupportActionBar(app_bar);
+		app_bar.setTitleTextColor(Color.WHITE);
+		getSupportActionBar().setHomeButtonEnabled(true);
+		getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu aaa)
+	{
+
+		MenuInflater  mimenu= getMenuInflater();
+		mimenu.inflate(R.menu.my_menu,aaa);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item)
+	{
+
+		if(item.getItemId()==android.R.id.home){
+
+			startActivity(new  Intent(this, MainActivity.class));
+		}
+		return true;
+	}
+
+	
+	
+
+
+
+}
---
+++ app/src/main/java/com/UAD/baseforapp/MainActivity.java
@@ -0,0 +1,46 @@
+package com.UAD.baseforapp;
+import android.support.v7.app.*;
+import android.os.*;
+import android.support.v7.appcompat.*;
+
+import android.system.*;
+
+import android.support.v7.widget.*;
+import android.view.*;
+import android.graphics.*;
+import android.content.*;
+public class MainActivity extends ActionBarActivity
+{  
+
+//private Toolbar app_bar;
+
+	@Override 
+	protected void onCreate(Bundle savedInstanceState) {  super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+//		app_bar=(Toolbar) findViewById(R.id.app_bar);
+//	setSupportActionBar(app_bar);
+//	app_bar.setTitleTextColor(Color.WHITE);
+//}
+//
+//@Override
+//public boolean onCreateOptionsMenu(Menu aaa)
+//{
+//
+//MenuInflater  mimenu= getMenuInflater();
+//mimenu.inflate(R.menu.my_menu,aaa);
+//	return true;
+//}
+//
+//@Override
+//public boolean onOptionsItemSelected(MenuItem item)
+//{
+//
+//if(item.getItemId()==R.id.about){
+//	
+//	startActivity(new  Intent(this, About.class));
+//}
+//	return true;
+	}
+
+
+}
